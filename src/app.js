a526565e5767f5019298e49195812a25125b24cb
HaiqalRahman12 <haiqal04pku@gmail.com>
2025-01-07 22:14:09 +0700

    haikalawal

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const HOST = process.env.HOST || '0.0.0.0';
+const HOST = process.env.HOST || 'localhost';
 
 app.use(cors()); 
 app.use(express.json());
